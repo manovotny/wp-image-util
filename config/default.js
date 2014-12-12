b98712b97e3ebeed5f4b4be2640e6d101ef69ead
Michael Novotny <manovotny@gmail.com>
2014-12-12 14:23:52 -0600

    Bumps version for release.

--- config/default.js
+++ config/default.js
@@ -25,7 +25,7 @@ module.exports = (function () {
             slug: 'wp-image-util',
             type: 'plugin', // Should be `plugin` or `theme`.
             url: 'https://github.com/manovotny/wp-image-util',
-            version: '1.0.2'
+            version: '1.1.0'
         }
     };
 
